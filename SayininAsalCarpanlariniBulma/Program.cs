b204e7fd8738d8ef3aaa95560f4bf3bcf12f8de0
eesrapekerr <33705100+eesrapekerr@users.noreply.github.com>
2019-11-22 12:06:08 +0300

    Update Program.cs

--- SayininAsalCarpanlariniBulma/Program.cs
+++ SayininAsalCarpanlariniBulma/Program.cs
@@ -2,11 +2,68 @@
 
 namespace SayininAsalCarpanlariniBulma
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int asal = 2;
+            //int kontrol = 0;
+
+            //Console.Write("1'den büyük tam sayı giriniz:");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //while (sayi > 1)
+            //{
+            //    if (sayi % asal == 0)
+            //    {
+            //        if (asal != kontrol)
+            //        {
+            //            kontrol = asal;
+            //            Console.Write(asal + "-");
+            //            sayi = sayi / asal;
+            //        }
+            //        else
+            //        {
+            //            sayi = sayi / asal;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        asal++;
+            //    }
+
+            //}
+            //Console.ReadKey();
+
+            int asal = 2;
+            int kontrol = 0;
+            int sayi = 15;
+
+            while (sayi > 1)
+            {
+
+                if (sayi % asal == 0)
+                {
+                    if (asal != kontrol)
+                    {
+                        kontrol = asal;
+                        Console.Write(asal + " - ");
+                        sayi = sayi / asal;
+                    }
+                    else
+                    {
+                        sayi = sayi / asal;
+                    }
+                }
+                else
+                {
+                    asal++;
+                }
+
+            }
+            Console.ReadLine();
+            
+            
         }
     }
 }
+
