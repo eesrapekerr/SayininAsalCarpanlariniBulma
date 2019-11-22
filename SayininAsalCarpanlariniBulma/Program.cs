11b1aee56c50e09e1f908e898a676fc0d63b4152
eesrapekerr <33705100+eesrapekerr@users.noreply.github.com>
2019-11-22 09:57:33 +0300

    Add project files.

---
+++ SayininAsalCarpanlariniBulma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SayininAsalCarpanlariniBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
